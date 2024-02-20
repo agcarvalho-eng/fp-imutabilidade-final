85cd507d68b1d1748383521e3654de0877207451
agcarvalho.eng <agcarvalho.eng@gmail.com>
2024-02-20 17:14:11 -0300

    Versão final 20-02-2024 17:14hs.

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,9 +1,17 @@
+import exercicios.Cidade;
+import exercicios.CidadeRecord;
+import exercicios.PessoaRecord;
+import exercicios.PessoaRecordShallow;
+
 /**
  * Classe para você testar suas implementações.
  * Veja o README para mais detalhes.
  */
 public class Principal {
     public static void main(final String[] args) {
-
+        var pessoaRecordShallow = new PessoaRecordShallow("Manoel", new Cidade("Palmas"));
+        pessoaRecordShallow.cidade().setNome("Gurupi");
+        System.out.println(pessoaRecordShallow.nome());
+        System.out.println(pessoaRecordShallow.cidade().getNome());
     }
 }
